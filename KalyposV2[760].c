807c079115a20abf13c85174a7d2a5ea7c88b4f5
Unknown <rykergermain@gmail.com>
2018-02-20 16:26:57 -0600

    Same as the last code except cleaned up.

--- KalyposV2[760].c
+++ KalyposV2[760].c
@@ -45,13 +45,11 @@
 	deg = deg*265/4095;
 }
 */
-void pre_auton()
-{
+void pre_auton(){
   bStopTasksBetweenModes = true;
 }
 
-task autonomous()
-{
+task autonomous(){
 
 motor [Lift2R] = -127;
 motor [Lift2L] = -127;
@@ -91,7 +89,6 @@ motor[Left ] = 0;
 */
 
 
-
 }
 
 task usercontrol()
@@ -111,91 +108,90 @@ task usercontrol()
     int deadzone   = 5;
 
 
-/////////// Arcade Drive////////////////////////
-  	if(abs(vexRT[Ch3]) >deadzone){
-			leftstickY = vexRT[Ch3];}
-		else{
-			leftstickY = 0;
-}
+/////////////// Arcade Drive ///////////////
 
-		if(abs(vexRT[Ch4]) > deadzone){
+  	if(abs(vexRT[Ch3]) >deadzone)
+			leftstickY = vexRT[Ch3];
+
+		else
+			leftstickY = 0;
+
+		if(abs(vexRT[Ch4]) > deadzone)
 			leftstickX = vexRT[Ch4];
-}
-		else{
+
+		else
 			leftstickX =0 ;
-}
 
 		motor[Left] = leftstickY + leftstickX;
 		motor[Right]= leftstickY - leftstickX;
-/////////// Arcade Drive///////////////////////
 
-//////// First Lift Movement///////////////////
+/////////////// First Lift Movement ///////////////
+
 		if (Liftup == 1){
 			motor [LiftR] = 127;
 			motor [LiftL] = 127;
 			motor [LIFTR] = 127;
 			motor [LIFTL] = 127;
-}
+		}
 		else if (Liftdown  == 1){
 			motor [LiftR] = -127;
 			motor [LiftL] = -127;
 			motor [LIFTR] = -127;
 			motor [LIFTL] = -127;
-}
-			else {
+		}
+		else {
 				motor[LiftR] = 10;
 				motor[LiftL] = -10;
 			  motor [LIFTR] = -10;
 			  motor [LIFTL] = 10;
-}
-//////// First Lift Movement///////////////////
+		}
 
-//////// Second lift with ch2//////////////////
-  	 if (Lift2M >=15){
+/////////////// Second Lift In Ch2 ///////////////
+
+		if (Lift2M >=15){
       motor [Lift2L] = Lift2M;
       motor [Lift2R] = Lift2M;
-}
+	  }
     	else if (Lift2M <=-15){
       motor  [Lift2L] = Lift2M;
       motor  [Lift2R] = Lift2M;
-}
+		}
     	else{
       motor [Lift2L] = 0;
       motor [Lift2R] = 0;
-}
+		}
 
-//////// Claw Control/////////////////////////
-		if (Clawopen == 1){
+/////////////// Claw Control ///////////////
+
+		if (Clawopen == 1)
 			motor [Claw] = 127;
-}
-		else if (Clawclose == 1){
-			motor [Claw] = -127;
-}
-		else{
-			motor [Claw] = 0;
-}
-//////// Claw Control/////////////////////////
 
-//////// Second lift//////////////////////////
+		else if (Clawclose == 1)
+			motor [Claw] = -127;
+
+		else
+			motor [Claw] = 0;
+
+/////////////// Second lift ///////////////
+
 		if (vertical==1){
 			motor [Lift2R] =127;
 			motor [Lift2L] =127;
 			wait(0.5);
 			motor [Lift2R] =-15;
 			motor [Lift2L] =-15;
-}
-//////// Second lift/////////////////////////
+		}
 
-//////// Mobile Goal/////////////////////////
-		if(MobileUp == 1){
+/////////////// Mobile Goal ///////////////
+
+		if(MobileUp == 1)
 			motor [Mobile] = 127;
-}
-		else if (MobileDown == 1){
+
+		else if (MobileDown == 1)
 			motor [Mobile] = -127;
-}
-		else{
+
+		else
 			motor [Mobile] = 0;
-//////// Mobile Goal/////////////////////////
-	}
+
  }
 }
