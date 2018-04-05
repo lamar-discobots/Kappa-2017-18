f09c709c58712219db56b373c183a85462bcd198
Victor Ponce <SLIME1230@gmail.com>
2018-04-05 10:39:18 -0500

    auton fixed

--- CalypsoV3.c
+++ CalypsoV3.c
@@ -31,18 +31,18 @@
 
 void pre_auton()
 {
-  // Set bStopTasksBetweenModes to false if you want to keep user created tasks
-  // running between Autonomous and Driver controlled modes. You will need to
-  // manage all user created tasks if set to false.
-  bStopTasksBetweenModes = true;
+	// Set bStopTasksBetweenModes to false if you want to keep user created tasks
+	// running between Autonomous and Driver controlled modes. You will need to
+	// manage all user created tasks if set to false.
+	bStopTasksBetweenModes = true;
 
 	// Set bDisplayCompetitionStatusOnLcd to false if you don't want the LCD
 	// used by the competition include file, for example, you might want
 	// to display your team name on the LCD in this function.
 	// bDisplayCompetitionStatusOnLcd = false;
 
-  // All activities that occur before the competition starts
-  // Example: clearing encoders, setting servo positions, ...
+	// All activities that occur before the competition starts
+	// Example: clearing encoders, setting servo positions, ...
 }
 
 /*---------------------------------------------------------------------------*/
@@ -57,252 +57,332 @@ void pre_auton()
 
 task autonomous(){
 
-/////////////// Auton 1 (Start against right wall) ///////////////
-
-/*
-motor[Lift2R]= -127;
-motor[Lift2L]= -127;
-motor[LiftR] = 127;
-motor[LIFTR] = 127;
-motor[LiftL] = 127;
-motor[LIFTL] = 127;
-motor[Mobile]= -127;
-wait (0.5);
-motor[Right] = 84.5;
-motor[Left]  = 84.5;
-wait (1.0);
-motor[Lift2R]= 25;
-motor[Lift2L]= 25;
-motor[LiftR] = 0;
-motor[LIFTR] = 0;
-motor[LiftL] = 0;
-motor[LIFTL] = 0;
-motor[Mobile]= 127;
-wait (1.5);
-motor[Mobile]= 0;
-motor[Right] = 0;
-motor[Left]  = 0;
-wait (1.0);
-motor[Mobile] = -127;
-wait(1.0);
-motor[LiftR] = -127;
-motor[LIFTR] = -127;
-motor[LiftL] = -127;
-motor[LIFTL] = -127;
-wait(1.0);
-motor[Right] = -127;
-motor[Left]  = -127;
-motor[Mobile]= 0;
-wait(1.0);
-motor[Lift2R]= 127;
-motor[Lift2L]= 127;
-motor[Right] = 64.5;    // Change to switch arena sides
-motor[Left]  = -64.5;   // Change to switch arena sides
-motor[LiftR] = 127;
-motor[LIFTR] = 127;
-motor[LiftL] = 127;
-motor[LIFTL] = 127;
-wait(2.0);
-motor[Right] = 127;
-motor[Left ] = 127;
-wait(1.0);
-motor[Mobile]= 127;
-motor[Right] = 127;
-motor[Left]  = 127;
-wait(1.5);
-motor[LiftR] = 0;
-motor[LIFTR] = 0;
-motor[LiftL] = 0;
-motor[LIFTL] = 0;
-motor[Right] = -127;
-motor[Left]  =  -127;
-motor[Mobile]= -127;
+	/////////////// Auton 1 (Start against right wall) ///////////////
 
 
-/////////////// Auton 1 (Against right wall) ///////////////
 
-/////////////// Auton 2 (Start against left wall) ///////////////
+	///////////////////////////////The ultima auton/////////////////////
+	motor [Lift2R] =  -127;
+	motor [Lift2L] =  -127;
+	motor [LiftR]  =  127;
+	motor [LIFTR]  =  127;
+	motor [LiftL]  =  127;
+	motor [LIFTL]  =  127;
+	motor [Claw ]  =  -127;
+	wait  (2.0);
+	motor [LiftR]  =  0;
+	motor [LiftL]  =  0;
+	motor [LIFTR]  =  0;
+	motor [LIFTL]  =  0;
+	motor [Lift2R] =  64.5;
+	motor [Lift2L] =  64.5;
+	wait  (0.6);
+	motor [Right]  =  127;
+	motor [Left ]  =  127;
+	wait  (0.7);
+	motor [Right]  =  0;
+	motor [Left]   =  0;
+	motor [Lift2R] = 32;
+	motor [Lift2L] = 32;
+	wait  (1.2);
+	motor [Lift2R] =  25;
+	motor [Lift2L] =  25;
+	motor [LIFTR]  = -127;
+	motor [LIFTL]  = -127;
+	motor [LIFTR]  = -127;
+	motor [LIFTL]  = -127;
+	wait (1.0);
+	motor [Claw]   =  127;
+	wait (1.0);
+	motor [Claw]   =  0;
+	wait (0.3);
+	motor [Right]  = 0;
+	motor [Left ]  = 0;
+	motor [LiftR]  = 0;
+	motor [LiftL]  = 0;
+	motor [LIFTR]  = 0;
+	motor [LIFTL]  = 0;
+	motor [Right]  = 0;
+	wait  (1.0);
+	motor [Left ]  = -127;
+	motor [Right]  = -127;
+	wait  (0.3);
+	motor [Mobile] = -127;
+	motor [Right]  = -64.5;
+	motor [Left ]  = 64.5;
+	wait  (1.1);
+	motor [Mobile] = -127;
+	wait  (0.5);
+	motor [Right]  = 127;
+	motor [Left] 	 = 127;
+	motor [LiftR]  = 64.5;
+	motor [LiftL]  = 64.5;
+	motor [LIFTR]  = 64.5;
+	motor [LIFTL]  = 64.5;
+	wait (2.5);
+	motor [Mobile] = -127;
+	motor [Right]  = 0;
+	motor [Left]   = 0;
+	wait  (2.0);
+	motor [Right]  =  -127;
+	motor [Left] 	 =  -127;
+	wait (2.0);
+	motor [Right]  = 64.5;
+	motor [Left]   = -64.5;
+	wait  (1.5);
+	motor [Right]  = 127;
+	motor [Left]	 = 127;
+	motor [Mobile] = 127;
+	wait (0.5);
+	motor [Left]   = -127;
+	motor [Right]  = -127;
+	motor [Mobile] = 0;
+	///////////////////////////////The ultimate auton///////////////////
+	/*
+	motor[Lift2R]= -127;
+	motor[Lift2L]= -127;
+	motor[LiftR] = 127;
+	motor[LIFTR] = 127;
+	motor[LiftL] = 127;
+	motor[LIFTL] = 127;
+	motor[Mobile]= -127;
+	wait (0.5);
+	motor[Right] = 84.5;
+	motor[Left]  = 84.5;
+	wait (1.0);
+	motor[Lift2R]= 25;
+	motor[Lift2L]= 25;
+	motor[LiftR] = 0;
+	motor[LIFTR] = 0;
+	motor[LiftL] = 0;
+	motor[LIFTL] = 0;
+	motor[Mobile]= 127;
+	wait (1.5);
+	motor[Mobile]= 0;
+	motor[Right] = 0;
+	motor[Left]  = 0;
+	wait (1.0);
+	motor[Mobile] = -127;
+	wait(1.0);
+	motor[LiftR] = -127;
+	motor[LIFTR] = -127;
+	motor[LiftL] = -127;
+	motor[LIFTL] = -127;
+	wait(1.0);
+	motor[Right] = -127;
+	motor[Left]  = -127;
+	motor[Mobile]= 0;
+	wait(1.0);
+	motor[Lift2R]= 127;
+	motor[Lift2L]= 127;
+	motor[Right] = 64.5;    // Change to switch arena sides
+	motor[Left]  = -64.5;   // Change to switch arena sides
+	motor[LiftR] = 127;
+	motor[LIFTR] = 127;
+	motor[LiftL] = 127;
+	motor[LIFTL] = 127;
+	wait(2.0);
+	motor[Right] = 127;
+	motor[Left ] = 127;
+	wait(1.0);
+	motor[Mobile]= 127;
+	motor[Right] = 127;
+	motor[Left]  = 127;
+	wait(1.5);
+	motor[LiftR] = 0;
+	motor[LIFTR] = 0;
+	motor[LiftL] = 0;
+	motor[LIFTL] = 0;
+	motor[Right] = -127;
+	motor[Left]  =  -127;
+	motor[Mobile]= -127;
 
-/*
-motor[Lift2R]= -127;
-motor[Lift2L]= -127;
-motor[LiftR] = 127;
-motor[LIFTR] = 127;
-motor[LiftL] = 127;
-motor[LIFTL] = 127;
-motor[Right] = 64.5;
-motor[Left]  = 64.5;
-wait (1.0);
-motor[Lift2R]= 25;
-motor[Lift2L]= 25;
-motor[LiftR] = 0;
-motor[LIFTR] = 0;
-motor[LiftL] = 0;
-motor[LIFTL] = 0;
-motor[Mobile]= 127;
-wait (1.5);
-motor[Mobile]= 0;
-motor[Right] = 0;
-motor[Left]  = 0;
-wait (1.0);
-motor[Mobile] = -127;
-wait(1.0);
-motor[LiftR] = -127;
-motor[LIFTR] = -127;
-motor[LiftL] = -127;
-motor[LIFTL] = -127;
-wait(1.0);
-motor[Right] = -127;
-motor[Left]  = -127;
-motor[Mobile]= 0;
-wait(1.0);
-motor[Lift2R]= 127;
-motor[Lift2L]= 127;
-motor[Right] = -64.5;
-motor[Left]  = 64.5;
-motor[LiftR] = 127;
-motor[LIFTR] = 127;
-motor[LiftL] = 127;
-motor[LIFTL] = 127;
-wait(2.0);
-motor[Right] = 127;
-motor[Left ] = 127;
-wait(1.0);
-motor[Mobile]= 127;
-motor[Right] = 127;
-motor[Left]  = 127;
-wait(1.5);
-motor[LiftR] = 0;
-motor[LIFTR] = 0;
-motor[LiftL] = 0;
-motor[LIFTL] = 0;
-motor[Right] = -127;
-motor[Left] =  -127;
-motor[Mobile] = -127;
-*/
 
-/////////////// Auton 2 (Against left wall) ///////////////
+	/////////////// Auton 1 (Against right wall) ///////////////
 
-/////////////// Auton 3 (High Stack) ///////////////  (Start against which side??)
+	/////////////// Auton 2 (Start against left wall) ///////////////
 
-motor [Lift2R] =  -127;
-motor [Lift2L] =  -127;
-motor [LiftR]  =  127;
-motor [LIFTR]  =  127;
-motor [LiftL]  =  127;
-motor [LIFTL]  =  127;
-motor [Claw ]  =  127;
-wait  (2.0);
-motor [LiftR]  =  0;
-motor [LiftL]  =  0;
-motor [LIFTR]  =  0;
-motor [LIFTL]  =  0;
-motor [Lift2R] =  127;
-motor [Lift2L] =  127;
-wait  (0.5);
-motor [Right]  =  127;
-motor [Left ]  =  127;
-wait  (2.0);
-motor [Right]  =  0;
-motor [Left]   =  0;
-motor [Lift2R] = 15;
-motor [Lift2L] = 156;
-wait  (1.2);
-motor [Lift2R] =  15;
-motor [Lift2L] =  15;
-motor [LIFTR]  = -127;
-motor [LIFTL]  = -127;
-motor [LIFTR]  = -127;
-motor [LIFTL]  = -127;
-wait (1.0);
-motor [Claw]   =  -127;
-wait (1.0);
-motor [Claw]   =  0;
-wait (0.5);
-motor [Right]  = -64.5;
-motor [Left ]  = -64.5;
-wait (0.5);
-motor [Right]  = 0;
-motor [Left ]  = 0;
-motor [LiftR]  = 0;
-motor [LiftL]  = 0;
-motor [LIFTR]  = 0;
-motor [LIFTL]  = 0;
-motor [Right]  = 0;
-wait  (1.0);
-motor [Left ]  = -127;
-motor [Right]  = -127;
-wait  (0.2);
-motor [LiftR]  = 127;
-motor [LiftL]  = 127;
-motor [LIFTR]  = 127;
-motor [LIFTL]  = 127;
-motor [Mobile] = 127;
-wait  (1.0);
-motor [Mobile] = 0;
-motor [LiftR]  = 0;
-motor [LiftL]  = 0;
-motor [LIFTR]  = 0;
-// second part of the auton/////////
-/*
-motor [LIFTL]  = 0;
-motor [Right]  = -64.5;
-motor [Left ]  =  64.5;
-wait  (2.0);
-motor [Right]  =  0;
-motor [Left ]  =  0;
-wait  (0.5);
-motor [Right]  =  127;
-motor [Left ]  =  127;
-wait  (2.0);
-motor[Lift2R]= 25;
-motor[Lift2L]= 25;
-motor[LiftR] = 0;
-motor[LIFTR] = 0;
-motor[LiftL] = 0;
-motor[LIFTL] = 0;
-motor[Mobile]= 127;
-wait (1.5);
-motor[Mobile]= 0;
-motor[Right] = 0;
-motor[Left]  = 0;
-wait (1.0);
-motor[Mobile] = -127;
-wait(1.0);
-motor[LiftR] = -127;
-motor[LIFTR] = -127;
-motor[LiftL] = -127;
-motor[LIFTL] = -127;
-wait(1.0);
-motor[Right] = -127;
-motor[Left]  = -127;
-motor[Mobile]= 0;
-wait(1.0);
-motor[Lift2R]= 127;
-motor[Lift2L]= 127;
-motor[Right] = 64.5;    // Change to switch arena sides
-motor[Left]  = -64.5;   // Change to switch arena sides
-motor[LiftR] = 127;
-motor[LIFTR] = 127;
-motor[LiftL] = 127;
-motor[LIFTL] = 127;
-wait(2.0);
-motor[Right] = 127;
-motor[Left ] = 127;
-wait(1.0);
-motor[Mobile]= 127;
-motor[Right] = 127;
-motor[Left]  = 127;
-wait(1.5);
-motor[LiftR] = 0;
-motor[LIFTR] = 0;
-motor[LiftL] = 0;
-motor[LIFTL] = 0;
-motor[Right] = -127;
-motor[Left] =  -127;
-motor[Mobile] = -127;
-*/
-/////////////// Auton 3 (High Stack) ///////////////
+	/*
+	motor[Lift2R]= -127;
+	motor[Lift2L]= -127;
+	motor[LiftR] = 127;
+	motor[LIFTR] = 127;
+	motor[LiftL] = 127;
+	motor[LIFTL] = 127;
+	motor[Right] = 64.5;
+	motor[Left]  = 64.5;
+	wait (1.0);
+	motor[Lift2R]= 25;
+	motor[Lift2L]= 25;
+	motor[LiftR] = 0;
+	motor[LIFTR] = 0;
+	motor[LiftL] = 0;
+	motor[LIFTL] = 0;
+	motor[Mobile]= 127;
+	wait (1.5);
+	motor[Mobile]= 0;
+	motor[Right] = 0;
+	motor[Left]  = 0;
+	wait (1.0);
+	motor[Mobile] = -127;
+	wait(1.0);
+	motor[LiftR] = -127;
+	motor[LIFTR] = -127;
+	motor[LiftL] = -127;
+	motor[LIFTL] = -127;
+	wait(1.0);
+	motor[Right] = -127;
+	motor[Left]  = -127;
+	motor[Mobile]= 0;
+	wait(1.0);
+	motor[Lift2R]= 127;
+	motor[Lift2L]= 127;
+	motor[Right] = -64.5;
+	motor[Left]  = 64.5;
+	motor[LiftR] = 127;
+	motor[LIFTR] = 127;
+	motor[LiftL] = 127;
+	motor[LIFTL] = 127;
+	wait(2.0);
+	motor[Right] = 127;
+	motor[Left ] = 127;
+	wait(1.0);
+	motor[Mobile]= 127;
+	motor[Right] = 127;
+	motor[Left]  = 127;
+	wait(1.5);
+	motor[LiftR] = 0;
+	motor[LIFTR] = 0;
+	motor[LiftL] = 0;
+	motor[LIFTL] = 0;
+	motor[Right] = -127;
+	motor[Left] =  -127;
+	motor[Mobile] = -127;
+	*/
+
+	/////////////// Auton 2 (Against left wall) ///////////////
+
+	/////////////// Auton 3 (High Stack) ///////////////  (Start pn any side of the field)
+	/*
+	motor [Lift2R] =  -127;
+	motor [Lift2L] =  -127;
+	motor [LiftR]  =  127;
+	motor [LIFTR]  =  127;
+	motor [LiftL]  =  127;
+	motor [LIFTL]  =  127;
+	motor [Claw ]  =  127;
+	wait  (2.0);
+	motor [LiftR]  =  0;
+	motor [LiftL]  =  0;
+	motor [LIFTR]  =  0;
+	motor [LIFTL]  =  0;
+	motor [Lift2R] =  127;
+	motor [Lift2L] =  127;
+	wait  (0.5);
+	motor [Right]  =  127;
+	motor [Left ]  =  127;
+	wait  (2.0);
+	motor [Right]  =  0;
+	motor [Left]   =  0;
+	motor [Lift2R] = 15;
+	motor [Lift2L] = 156;
+	wait  (1.2);
+	motor [Lift2R] =  15;
+	motor [Lift2L] =  15;
+	motor [LIFTR]  = -127;
+	motor [LIFTL]  = -127;
+	motor [LIFTR]  = -127;
+	motor [LIFTL]  = -127;
+	wait (1.0);
+	motor [Claw]   =  -127;
+	wait (1.0);
+	motor [Claw]   =  0;
+	wait (0.5);
+	motor [Right]  = -64.5;
+	motor [Left ]  = -64.5;
+	wait (0.5);
+	motor [Right]  = 0;
+	motor [Left ]  = 0;
+	motor [LiftR]  = 0;
+	motor [LiftL]  = 0;
+	motor [LIFTR]  = 0;
+	motor [LIFTL]  = 0;
+	motor [Right]  = 0;
+	wait  (1.0);
+	motor [Left ]  = -127;
+	motor [Right]  = -127;
+	wait  (0.2);
+	motor [LiftR]  = 127;
+	motor [LiftL]  = 127;
+	motor [LIFTR]  = 127;
+	motor [LIFTL]  = 127;
+	motor [Mobile] = 127;
+	wait  (1.0);
+	motor [Mobile] = 0;
+	motor [LiftR]  = 0;
+	motor [LiftL]  = 0;
+	motor [LIFTR]  = 0;
+	/*
+	// second part of the auton/////////
+	/*
+	motor [LIFTL]  = 0;
+	motor [Right]  = -64.5;
+	motor [Left ]  =  64.5;
+	wait  (2.0);
+	motor [Right]  =  0;
+	motor [Left ]  =  0;
+	wait  (0.5);
+	motor [Right]  =  127;
+	motor [Left ]  =  127;
+	wait  (2.0);
+	motor[Lift2R]= 25;
+	motor[Lift2L]= 25;
+	motor[LiftR] = 0;
+	motor[LIFTR] = 0;
+	motor[LiftL] = 0;
+	motor[LIFTL] = 0;
+	motor[Mobile]= 127;
+	wait (1.5);
+	motor[Mobile]= 0;
+	motor[Right] = 0;
+	motor[Left]  = 0;
+	wait (1.0);
+	motor[Mobile] = -127;
+	wait(1.0);
+	motor[LiftR] = -127;
+	motor[LIFTR] = -127;
+	motor[LiftL] = -127;
+	motor[LIFTL] = -127;
+	wait(1.0);
+	motor[Right] = -127;
+	motor[Left]  = -127;
+	motor[Mobile]= 0;
+	wait(1.0);
+	motor[Lift2R]= 127;
+	motor[Lift2L]= 127;
+	motor[Right] = 64.5;    // Change to switch arena sides
+	motor[Left]  = -64.5;   // Change to switch arena sides
+	motor[LiftR] = 127;
+	motor[LIFTR] = 127;
+	motor[LiftL] = 127;
+	motor[LIFTL] = 127;
+	wait(2.0);
+	motor[Right] = 127;
+	motor[Left ] = 127;
+	wait(1.0);
+	motor[Mobile]= 127;
+	motor[Right] = 127;
+	motor[Left]  = 127;
+	wait(1.5);
+	motor[LiftR] = 0;
+	motor[LIFTR] = 0;
+	motor[LiftL] = 0;
+	motor[LIFTL] = 0;
+	motor[Right] = -127;
+	motor[Left] =  -127;
+	motor[Mobile] = -127;
+	*/
+	/////////////// Auton 3 (High Stack) ///////////////
 
 }
 
@@ -310,127 +390,127 @@ motor[Mobile] = -127;
 
 task usercontrol()
 {
-   while (true){
+	while (true){
 
-  	int Clawopen    			= vexRT(Btn8R);
-  	int Clawclose  				= vexRT(Btn8D);
-  	int MobileUp   		    = vexRT(Btn7D);
-  	int MobileDown			  = vexRT(Btn7L);
-  	int Liftdown   				= vexRT(Btn5D);
-  	int Liftup    				= vexRT(Btn5U);
-  	int Lift2M     				= vexRT(Ch2);
- // int vertical          = vexRT(Btn6U);
-  	int leftstickX;
-  	int leftstickY;
-  	int deadzone   				= 5;
+		int Clawopen    			= vexRT(Btn8D);
+		int Clawclose  				= vexRT(Btn8R);
+		int MobileUp   		    = vexRT(Btn7L);
+		int MobileDown			  = vexRT(Btn7D);
+		int Liftdown   				= vexRT(Btn5D);
+		int Liftup    				= vexRT(Btn5U);
+		int Lift2M     				= vexRT(Ch2);
+		// int vertical          = vexRT(Btn6U);
+		int leftstickX;
+		int leftstickY;
+		int deadzone   				= 5;
 
-/////////////// Arcade Drive ///////////////
+		/////////////// Arcade Drive ///////////////
 
-  	if (abs(vexRT[Ch3]) > deadzone){
+		if (abs(vexRT[Ch3]) > deadzone){
 			leftstickY = vexRT[Ch3];
-	 }
+		}
 		else {
 			leftstickY = 0;
-	 }
+		}
 
 		if (abs(vexRT[Ch4]) > deadzone){
 			leftstickX = vexRT[Ch4];
-	 }
+		}
 		else {
 			leftstickX =0 ;
-	 }
+		}
 
 		motor[Left] = leftstickY + leftstickX;
 		motor[Right]= leftstickY - leftstickX;
-/////////////// Arcade Drive ///////////////
+		/////////////// Arcade Drive ///////////////
 
-/////////////// First Lift Movement ///////////////
-    if (Liftup == 1){
-      motor [LiftR] = 127;
-      motor [LiftL] = 127;
-      motor [LIFTR] = 127;
-      motor [LIFTL] = 127;
-}
-    else if (Liftdown  == 1){
-      motor [LiftR] = -127;
-      motor [LiftL] = -127;
-      motor [LIFTR] = -127;
-      motor [LIFTL] = -127;
-}
-      else {
-        motor[LiftR] = 10;
-        motor[LiftL] = -10;
-        motor [LIFTR] = -10;
-        motor [LIFTL] = 10;
-}
-/////////////// First Lift Movement ///////////////
+		/////////////// First Lift Movement ///////////////
+		if (Liftup == 1){
+			motor [LiftR] = 127;
+			motor [LiftL] = 127;
+			motor [LIFTR] = 127;
+			motor [LIFTL] = 127;
+		}
+		else if (Liftdown  == 1){
+			motor [LiftR] = -127;
+			motor [LiftL] = -127;
+			motor [LIFTR] = -127;
+			motor [LIFTL] = -127;
+		}
+		else {
+			motor[LiftR] = 10;
+			motor[LiftL] = -10;
+			motor [LIFTR] = -10;
+			motor [LIFTL] = 10;
+		}
+		/////////////// First Lift Movement ///////////////
 
-/////////////// Second Lift Movement ///////////////
+		/////////////// Second Lift Movement ///////////////
 
-    if (Lift2M >=15){
-      motor [Lift2L] = Lift2M;
-      motor [Lift2R] = Lift2M;
-    }
-    else if (Lift2M <=-15){
-      motor  [Lift2L] = Lift2M;
-      motor  [Lift2R] = Lift2M;
-    }
-    else{
-      motor [Lift2L] = 15;
-      motor [Lift2R] = 15;
-    }
+		if (Lift2M >=15){
+			motor [Lift2L] = Lift2M;
+			motor [Lift2R] = Lift2M;
+		}
+		else if (Lift2M <=-15){
+			motor  [Lift2L] = Lift2M;
+			motor  [Lift2R] = Lift2M;
+		}
+		else{
+			motor [Lift2L] = 15;
+			motor [Lift2R] = 15;
+		}
 
-/////////////// Second Lift Movement ///////////////
+		/////////////// Second Lift Movement ///////////////
 
-/////////////// Mysteries ///////////////
-   /*
-    if (vertical ==1){
+		/////////////// Mysteries ///////////////
+		/*
+		if (vertical ==1){
 
-    motor [Lift2R] = 32.25;
-    motor [Lift2L] = 32.25;
-    wait (0.25);
-    motor [Lift2R] =0;
-    motor [Lift2L] =0;
-}
+		motor [Lift2R] = 32.25;
+		motor [Lift2L] = 32.25;
+		wait (0.25);
+		motor [Lift2R] =0;
+		motor [Lift2L] =0;
+		}
 
-    if (vertical ==1){
-      motor [Lift2R] =64.5;
-      motor [Lift2L] =64.5;
-      wait (0.5);
-      motor [Lift2R] =0;
-      motor [Lift2L] =0;
-}   */
+		if (vertical ==1){
+		motor [Lift2R] =64.5;
+		motor [Lift2L] =64.5;
+		wait (0.5);
+		motor [Lift2R] =0;
+		motor [Lift2L] =0;
+		}   */
 
-/////////////// Mysteries ///////////////
+		/////////////// Mysteries ///////////////
 
-/////////////// Claw Control ///////////////
+		/////////////// Claw Control ///////////////
 
-    if (Clawopen == 1)
-      motor [Claw] = 127;
+		if (Clawopen == 1)
+			motor [Claw] = 127;
 
-    else if (Clawclose == 1)
-      motor [Claw] = -127;
+		else if (Clawclose == 1)
+			motor [Claw] = -127;
 
-    else{
-      motor [Claw] = 0;
-    }
+		else{
+			motor [Claw] = 0;
+		}
 
-/////////////// Claw Control ///////////////
+		/////////////// Claw Control ///////////////
 
-/////////////// Mobile Goal ///////////////
+		/////////////// Mobile Goal ///////////////
 
- if (MobileUp == 1){
-      motor [Mobile] = 127;
-    }
-    else if (MobileDown == 1){
-      motor [Mobile] = -127;
-    }
-    else{
-      motor [Mobile] = 0;
-      motor [Mobile] = 0;
-    }
+		if (MobileUp == 1){
+			motor [Mobile] = 127;
+		}
+		else if (MobileDown == 1){
+			motor [Mobile] = -127;
+		}
+		else{
+			motor [Mobile] = 0;
+			motor [Mobile] = 0;
+		}
 
-/////////////// Mobile Goal ///////////////
+		/////////////// Mobile Goal ///////////////
 
-   }
+	}
 }
